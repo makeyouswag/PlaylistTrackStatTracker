cc4059aa35d5a9ff4c74655d1be4b35d069efc59
kalitka <zavorotnii.dr@gmail.com>
2024-10-21 17:39:19 +0700

    Proper settings class

---
+++ stat_tracker/settings.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pydantic.v1 import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DEBUG: bool = False
+    TESTING: bool = False
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
+
+    class Config:
+        env_file = "../.env"
+        env_file_encoding = "utf-8"
+        env_prefix = "TRACKER_"
+
+
+settings = Settings()
