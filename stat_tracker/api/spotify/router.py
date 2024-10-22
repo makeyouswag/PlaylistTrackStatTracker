d28652692279b04d5706da286f0c18cfe942d98c
kalitka <zavorotnii.dr@gmail.com>
2024-10-22 16:28:01 +0700

    Move spotify api router to a separate package

---
+++ stat_tracker/api/spotify/router.py
@@ -0,0 +1,41 @@
+import io
+
+from fastapi import APIRouter, Query
+from starlette.responses import FileResponse, StreamingResponse
+
+from stat_tracker.api.spotify.schemas import CamelotKey
+from stat_tracker.integrations.spotify.track_features import get_playlist_tracks
+from stat_tracker.services.csv_exporter import get_track_table_buffer
+from stat_tracker.services.utils import find_tracks_by_tune
+
+router = APIRouter(prefix="/spotify")
+
+
+@router.post("/track-table")
+def get_track_table(playlist_id: str) -> StreamingResponse:
+    tracks = get_playlist_tracks(playlist_id)
+    filename = f"{playlist_id}.csv"
+    return StreamingResponse(
+        get_track_table_buffer(tracks),
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename={filename}"},
+    )
+
+
+@router.post("/chosen-tune-tracks")
+def get_chosen_tune_track_table(
+    tune: CamelotKey,
+    playlist_ids: list[str] = Query(
+        None, description="Playlist ids to include in the search"
+    ),
+):
+    tracks = [
+        track
+        for playlist_id in playlist_ids
+        for track in get_playlist_tracks(playlist_id)
+    ]
+    return StreamingResponse(
+        get_track_table_buffer(find_tracks_by_tune(tracks, tune.value)),
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename={tune.value}.csv"},
+    )
---
+++ stat_tracker/api/spotify/schemas.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class CamelotKey(Enum):
+    # Major Keys
+    _1B = "1B"
+    _2B = "2B"
+    _3B = "3B"
+    _4B = "4B"
+    _5B = "5B"
+    _6B = "6B"
+    _7B = "7B"
+    _8B = "8B"
+    _9B = "9B"
+    _10B = "10B"
+    _11B = "11B"
+    _12B = "12B"
+
+    # Minor Keys
+    _1A = "1A"
+    _2A = "2A"
+    _3A = "3A"
+    _4A = "4A"
+    _5A = "5A"
+    _6A = "6A"
+    _7A = "7A"
+    _8A = "8A"
+    _9A = "9A"
+    _10A = "10A"
+    _11A = "11A"
+    _12A = "12A"
